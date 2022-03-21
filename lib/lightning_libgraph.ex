530f62289dd9c86538d4f70dd3877c2d7f097240
RooSoft <marc@roosoft.com>
2022-03-21 15:39:23 -0400

    create the graph

--- lib/lightning_libgraph.ex
+++ lib/lightning_libgraph.ex
@@ -1,18 +1,39 @@
 defmodule LightningLibgraph do
-  @moduledoc """
-  Documentation for `LightningLibgraph`.
-  """
+  @priv_dir "#{:code.priv_dir(:lightning_libgraph)}"
+  @csv_dir @priv_dir <> "/lightning/"
 
-  @doc """
-  Hello world.
+  def start do
+    Graph.new()
+    |> import_nodes()
+    |> import_channels()
+    |> Graph.info()
+  end
 
-  ## Examples
+  defp import_nodes(g) do
+    File.stream!(@csv_dir <> "nodes.csv")
+    |> CSV.decode(separator: ?,, headers: true)
+    |> Enum.reduce(g, fn row, g ->
+      {:ok, %{"pub_key" => pub_key, "alias" => node_alias, "color" => color}} = row
 
-      iex> LightningLibgraph.hello()
-      :world
+      g
+      |> Graph.add_vertex(pub_key)
+      |> Graph.label_vertex(pub_key, %{alias: node_alias, color: color})
+    end)
+  end
 
-  """
-  def hello do
-    :world
+  defp import_channels(g) do
+    File.stream!(@csv_dir <> "channels.csv")
+    |> CSV.decode(separator: ?,, headers: true)
+    |> Enum.reduce(g, fn row, g ->
+      {:ok,
+       %{
+         "channel_id" => id,
+         "node1_pub" => node1_pub,
+         "node2_pub" => node2_pub
+       }} = row
+
+      g
+      |> Graph.add_edge(Graph.Edge.new(node1_pub, node2_pub, label: id))
+    end)
   end
 end
