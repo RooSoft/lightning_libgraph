254cecf1b6b684f07a57ab39bacce896fcf0e3b0
RooSoft <marc@roosoft.com>
2022-03-23 18:35:19 -0400

    transformed GraphDownloader into a GenServer

--- lib/lightning_libgraph.ex
+++ lib/lightning_libgraph.ex
@@ -2,11 +2,41 @@ defmodule LightningLibgraph do
   alias LightningLibgraph.Lnd
 
   def load_graph do
+    Lnd.GraphDownloader.start_link(nil)
+    Lnd.GraphDownloader.subscribe()
+
     Graph.new()
     |> Lnd.GraphDownloader.load(
       Application.fetch_env!(:lightning_libgraph, :cert),
       Application.fetch_env!(:lightning_libgraph, :macaroon),
       Application.fetch_env!(:lightning_libgraph, :url)
     )
+
+    get_messages()
+  end
+
+  def get_messages() do
+    receive do
+      {:graph_downloader, :load, {:downloading}} ->
+        IO.inspect("downloading......")
+        get_messages()
+
+      {:graph_downloader, :load, {:parsing}} ->
+        IO.inspect("parsing......")
+        get_messages()
+
+      {:graph_downloader, :load, {:importing_nodes}} ->
+        IO.inspect("importing nodes......")
+        get_messages()
+
+      {:graph_downloader, :load, {:importing_channels}} ->
+        IO.inspect("importing channels.....")
+        get_messages()
+
+      {:graph_downloader, :load, {:done, final_graph}} ->
+        IO.inspect("done!!!")
+
+        final_graph
+    end
   end
 end
--- lib/lnd/graph_downloader.ex
+++ lib/lnd/graph_downloader.ex
@@ -1,31 +1,72 @@
 defmodule LightningLibgraph.Lnd.GraphDownloader do
+  use GenServer
+
   alias LightningLibgraph.Lnd.GraphDownloader.Channels
   alias LightningLibgraph.Lnd.GraphDownloader.Nodes
 
+  def start_link(_) do
+    GenServer.start_link(
+      __MODULE__,
+      %{}
+      |> Map.put(:subscribers, []),
+      name: __MODULE__
+    )
+  end
+
+  def stop(reason \\ :normal, timeout \\ :infinity) do
+    GenServer.stop(__MODULE__, reason, timeout)
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
   def load(g, cert, macaroon, url) do
-    IO.puts("downloading...")
+    GenServer.cast(__MODULE__, {:load, {g, cert, macaroon, url}})
+  end
+
+  def subscribe() do
+    GenServer.call(__MODULE__, {:subscribe, self()})
+  end
+
+  @impl true
+  def handle_call({:subscribe, pid}, _from, state) do
+    {
+      :reply,
+      :ok,
+      state
+      |> Map.put(:subscribers, [pid | state.subscribers])
+    }
+  end
+
+  @impl true
+  def handle_cast({:load, {g, cert, macaroon, url}}, state) do
+    send_to_subscribers({:downloading}, state.subscribers, :load)
+
     lnd_graph = get_graph(cert, macaroon, url)
 
-    IO.puts("parsing...")
+    send_to_subscribers({:parsing}, state.subscribers, :load)
+
     graph = Jason.decode!(lnd_graph)
 
-    IO.puts("importing nodes...")
+    send_to_subscribers({:importing_nodes}, state.subscribers, :load)
 
     graph_with_nodes =
       graph["nodes"]
       |> Enum.filter(fn node -> node["last_update"] > 0 end)
       |> Nodes.insert(g)
 
-    IO.puts("importing channels...")
+    send_to_subscribers({:importing_channels}, state.subscribers, :load)
 
     final_graph =
       graph["edges"]
       |> Enum.filter(fn edge -> edge["last_update"] > 0 end)
       |> Channels.insert(graph_with_nodes)
 
-    IO.puts("done!")
+    send_to_subscribers({:done, final_graph}, state.subscribers, :load)
 
-    final_graph
+    {:noreply, state}
   end
 
   defp get_headers(macaroon_filename) do
@@ -50,5 +91,12 @@ defmodule LightningLibgraph.Lnd.GraphDownloader do
     case :httpc.request(:get, request, options, []) do
       {:ok, {{_v, 200, _m}, _h, body}} -> :erlang.list_to_binary(body)
     end
+  end
+
+  defp send_to_subscribers(payload, subscribers, topic) do
+    subscribers
+    |> Enum.each(fn subscriber ->
+      send(subscriber, {:graph_downloader, topic, payload})
+    end)
   end
 end
